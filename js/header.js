e54e1bb4400832f42e0a17fd62b6b55464c5fd61
santhosh156 <santhosh156@gmail.com>
2019-04-04 01:24:46 +0530

    Signin modal created and functionalities added

--- js/header.js
+++ js/header.js
@@ -13,23 +13,17 @@ var createPostBtn = document.getElementById("createPostBtn");
 
 var signUpclose = document.getElementById("signUpclose");
 
-//var span = document.getElementsByClassName("close")[0];
-
-//var span1 = document.getElementsByClassName("close")[1];
-
-//var span2 = document.getElementsByClassName("close")[2];
+var signInclose = document.getElementById("signInclose");
 
 // When the user clicks the button, open the modal
 signInBtn.onclick = function() {
+    signupmodal.style.display = "none";
     signinmodal.style.display = "block";
 }
 
 signUpBtn.onclick = function() {
+    signinmodal.style.display = "none";
     signupmodal.style.display = "block";
-}
-
-createPostBtn.onclick = function(){
-    createpostmodal.style.display = "block";
 }
 
 
@@ -38,13 +32,10 @@ signUpclose.onclick = function() {
     signupmodal.style.display = "none";
 }
 
-span1.onclick = function() {
+signInclose.onclick = function() {
     signinmodal.style.display = "none";
 }
 
-span2.onclick = function() {
-    createpostmodal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -55,23 +46,10 @@ window.onclick = function(event) {
     if (event.target == signupmodal) {
         signupmodal.style.display = "none";
     }
-
-    if (event.target == createpostmodal) {
-        createpostmodal.style.display = "none";
-    }
 }
 
 
 function signUpModel(){
-
     signinmodal.style.display = "none";
-
     signupmodal.style.display = "block";
-
-    var span1 = document.getElementsByClassName("close")[1];
-
-    span1.onclick = function() {
-        signupmodal.style.display = "none";
-    }
-
 }
